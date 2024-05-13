a739ef0aeaee17cacfe37a66dfa191d317e40918
omar alwafaii <omaralwafaii@gmail.com>
2024-05-13 11:57:21 +0300

    security updates

--- back/database.js
+++ back/database.js
@@ -1,12 +1,19 @@
+const dotenv = require('dotenv');
 const Pool = require("pg").Pool;
+dotenv.config();
+
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'omarwaf2002',
-    port: 5432,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_host,
+    database: process.env.DATABASE_database,
+    password: process.env.DATABASE_password,
+    port: process.env.DATABASE_port,
 });
+
+pool
+    .query(`SELECT * FROM ${table} WHERE id FROM 0 to 20`)
+
 pool.connect();
 
 module.exports = pool;
